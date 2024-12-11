5fcc7cdc0fb68d1f12320db77289179666d40b62
Nik Tomazic <nick.tomazic@gmail.com>
2024-12-11 16:10:22 +0100

    project init

---
+++ app/images/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from images.models import Image
+
+
+class UploadForm(forms.ModelForm):
+
+    class Meta:
+        model = Image
+        fields = ["title", "file"]
+        labels = {"file": "Image"}
